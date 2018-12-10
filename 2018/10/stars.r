49048a1f4a7529931b131abd963e5ada3b616dab
Johan Ask <jhnesk@gmail.com>
2018-12-11 00:02:53 +0100

    Remove hardcoded magic value from stars.r

--- 2018/10/stars.r
+++ 2018/10/stars.r
@@ -6,17 +6,18 @@ velocities = read.csv("vel.txt")
 par(mar = c(0,0,1,0))
 par(pch=19)
 
-minVar <- 1000000000000
+minVar <- var(positions[,1])
 
 i <- 0
 repeat {
 	positions <- positions + velocities
-
 	currentVar <- var(positions[,1])
 
 	if (minVar > currentVar) {
 		minVar <- currentVar
 	} else {
+		# If the variation starts to rise, last position was correct.
+		# Plot the coordinates.
 		plot(positions - velocities, asp=1, main=i)
 		break
 	}
