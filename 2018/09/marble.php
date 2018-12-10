f6482881771fde51c469b7f283f42cf938db2e34
Johan Ask <jhnesk@gmail.com>
2018-12-10 22:15:46 +0100

    Fix performance issue to solve part 2 of day 10
    Use linked list instead of arrays for much better performance.
    Unfortunately this triggered a bug in the garbage collector after
    ~60000 iterations. Disabling the gc worked.
    To run this you need more than 128M ram, so you need to increase
    memory_limit in php.ini.

--- 2018/09/marble.php
+++ 2018/09/marble.php
@@ -1,35 +1,56 @@
 #!/usr/bin/env php
 <?php
+gc_disable();
 
 $players = $argv[1];
 $maxMarble = $argv[2];
 $scores = array_fill(0, $players, 0);
 
-$marbles = array (0);
-$currentMarbleIndex=0;
+class Marble {
+	public $value;
+	public $previous;
+	public $next;
 
-for ($marble = 1; $marble < $maxMarble; $marble++) {
-	$player = $marble % $players;
-	if ($marble % 23 == 0) {
-		$newIndex = $currentMarbleIndex - 7;
-		if ($newIndex < 0) {
-			$newIndex = count($marbles)+$newIndex;
-		}
-		$scores[$player] += $marble;
-		$scores[$player] += $marbles[$newIndex];
-		array_splice($marbles, $newIndex, 1); // remove marble at index
-		if ($newIndex >= count($marbles)) {
-			$newIndex = 0;
-		}
-	} else {
-		$newIndex = $currentMarbleIndex+2;
-		if ($newIndex >= count($marbles)) {
-			$newIndex = $newIndex - count($marbles);
-		}
-		array_splice($marbles, $newIndex, 0, array($marble));
+	function __construct($value) {
+		$this->value = $value;
 	}
 
-	$currentMarbleIndex = $newIndex;
+	function remove() {
+		$this->next->previous = $this->previous;
+		$this->previous->next = $this->next;
+	}
+}
+
+$currentMarble = new Marble(0);
+$currentMarble->previous = $currentMarble;
+$currentMarble->next = $currentMarble;
+
+$size=1;
+
+for ($marble = 1; $marble <= $maxMarble; $marble++) {
+	$player = $marble % $players;
+	if ($marble % 23 == 0) {
+		for ($i = 0; $i < 7; $i++) {
+			$currentMarble = $currentMarble->previous;
+		}
+		$scores[$player] += $marble + $currentMarble->value;
+		$currentMarble->remove();
+		$toDelete = $currentMarble;
+		$currentMarble = $currentMarble->next;
+		$toDelete->next = NULL;
+		$toDelete->previous = NULL;
+		unset($toDelete);
+		$size--;
+	} else {
+		$newMarble = new Marble($marble);
+		$newMarble->next = $currentMarble->next->next;
+		$newMarble->previous = $currentMarble->next;
+
+		$currentMarble->next->next->previous = $newMarble;
+		$currentMarble->next->next = $newMarble;
+		$currentMarble = $newMarble;
+		$size++;
+	}
 }
 
 echo max($scores)."\n";
