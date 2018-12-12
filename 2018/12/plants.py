b54a558a308929f05621306eb450684c0d3ee796
Johan Ask <jhnesk@gmail.com>
2018-12-12 17:18:15 +0100

    Solve day 12 part 1 with perl
    Naive solution that won't work for part 2.

---
+++ 2018/12/plants.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+
+chomp(my @lines = <>);
+
+my $state = shift @lines;
+$state =~ s/initial state: //;
+shift @lines;
+
+my %rules = map { substr($_,0,5) => substr($_,-1,1) } @lines;
+
+my $firstPot = 0;
+
+for (my $generation=0; $generation<20; $generation++) {
+    # Prepend and append four empty pots to last state.
+    $state = "....".$state."....";
+    $firstPot -= 2; # only the first two makes it to the next state...
+
+    my $next_state = "";
+ 
+    my $length = length($state)-4;
+    for (my $i=0; $i<$length; $i++) {
+        my $key = substr($state, $i, 5);
+        $next_state .= $rules{$key};
+    }
+    $next_state =~ s/(.*)\.+$/$1/;
+    my $prev_length = length($next_state);
+    $next_state =~ s/^\.+(.*)/$1/;
+    $firstPot += $prev_length - length($next_state);
+    $state = $next_state;
+}
+
+my $sum = 0;
+my $value = $firstPot;
+for (split //, $state) {
+    if ($_ eq '#') {
+        $sum += $value;
+    }
+    $value++;
+}
+
+print "12.1: $sum\n";
+
