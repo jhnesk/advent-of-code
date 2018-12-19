282eff16af23f4f0984e9fddc3d4534bcbcacc68
Johan Ask <jhnesk@gmail.com>
2018-12-19 14:39:37 +0100

    Cleaning up R code and minor refactoring
    Just because I like clean code.

--- 2018/10/stars.r
+++ 2018/10/stars.r
@@ -1,29 +1,27 @@
 #!/usr/bin/env Rscript
 
-positions = read.csv("pos.txt")
-velocities = read.csv("vel.txt")
-
-
-minVar <- var(positions[,1])
-
-i <- 0
-repeat {
-	positions <- positions + velocities
-	currentVar <- var(positions[,1])
-
-	if (minVar > currentVar) {
-		minVar <- currentVar
-	} else {
-		# If the variation starts to rise, last position was correct.
-		# Plot the coordinates.
-		positions <- positions - velocities
+createImage <- function(positions, i) {
 		png(filename="stars.png")
 		par(mar = c(0,0,1,0))
 		par(pch=19)
 		plot(positions, ylim = rev(range(positions[1,])), asp=1, main=i)
 		dev.off()
-		break
-	}
-	i <- i + 1
 }
 
+positions = read.csv("pos.txt")
+velocities = read.csv("vel.txt")
+
+previousVariation <- var(positions[,1])
+
+i <- 0
+repeat {
+	positions <- positions + velocities
+	currentVariation <- var(positions[,1])
+
+	if (previousVariation < currentVariation) {
+		createImage(positions - velocities, i)
+		break
+	}
+	previousVariation <- currentVariation
+	i <- i + 1
+}
